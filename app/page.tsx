516c112e73f578f18ec1232dd76c997e847fb0c9
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2026-03-01 14:06:39 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,39 @@
 import Image from "next/image";
 
 export default function Home() {
+  
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="h-screen bg-gradient-to-br from-[#04022a] via-[#020012] to-[#090909] bg-url bg-[url('/images/bg.jpg')] text-white relative overflow-hidden bg-cover bg-center bg-no-repeat  ">
+
+   <div className="absolute  z-0 inset-0 top-1/2 left-[77%] -translate-x-1/2 w-full max-w-5xl px-4 text-center text-white/45  ">
+
+     {/*   <Image
+          src={"/images/robo-bg-layer.png"}
+          alt="Background"
+          layout="fill"
+          className="object-cover opacity-70  "
+        />*/}
+
+        <h1 className="font-bold text-9xl uppercase font-mono tracking-widest">VEC TOR</h1>
+      </div>
+      <div className="absolute top-1/4 left-[90%] -translate-x-1/2 z-20  w-full max-w-3xl px-4 text-center">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={"/images/robo.png"}
+          alt="Background"
+          width={300}
+          height={300}
+          className="object-cover opacity-100  rounded-lg shadow-lg"
+        
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      
+      </div>
+      <main className="relative z-20 font-medium pt-30  tracking-widest text-white  min-h-screen px-10 ">
+    
+    <h1 className="font-mono text-3xl pb-25 t uppercase "><span className="text-indigo-300">Welcome,</span><br /> to the future of stock <br/> market analysis and  <br /> <span className="text-olive-300">AI-powered trend <br/> prediction</span></h1>
+    <div className="max-w-xl border-2 border-white/20 rounded-lg p-6  bg-white/10 backdrop-blur-3xl  ">
+    <p className="  text-mauve-400 text-xs font-mono uppercase tracking-widest ">Analyze market trends and predict future movements with our,<br /> advanced AI-powered platform.</p>
+        <p className="  text-mauve-400 text-xs font-mono uppercase tracking-widest">Advanced stock market analysis and trend,<br /> prediction using machine learning algorithms to help investors ,<br />make smarter and faster decisions.</p>
+      </div> 
       </main>
     </div>
   );

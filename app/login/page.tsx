439c9d975cbe2b38b91d0fc5de0e305fa0d19fe5
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2026-03-01 18:50:01 +0530

    3nd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div>Login</div>
+    <div className="h-screen bg-gradient-to-br from-[#04022a] via-[#020012] to-[#090909] bg-url bg-[url('/images/bg.jpg')] text-white relative overflow-hidden bg-cover bg-center bg-no-repeat  ">
+     
+     
+     </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-gradient-to-br from-[#04022a] via-[#020012] to-[#090909] bg-url bg-[url('/images/bg.jpg')] text-white relative overflow-hidden bg-cover bg-center bg-no-repeat  ">
 
-    <div className="absolute  z-0 inset-0 top-1/2 left-[77%] -translate-x-1/2 w-full max-w-5xl px-4 text-center text-white/45  ">
+    <div className="absolute  z-0 inset-0 top-[40%] left-[70%] -translate-x-1/2 w-full max-w-5xl px-4 text-center text-white/45  ">
 
      {/*   <Image
           src={"/images/robo-bg-layer.png"}
@@ -14,9 +14,19 @@ export default function Home() {
           className="object-cover opacity-70  "
         />*/}
 
-        <h1 className="font-bold text-9xl uppercase font-mono tracking-widest">VEC TOR</h1>
+        <h1 className=" text-[10rem] uppercase font-mono tracking-widest">VEC TOR</h1>
       </div>
-      <div className="absolute top-[20%] left-[90%] -translate-x-1/2 z-20  w-full max-w-3xl px-4 text-center">
+        <div className="absolute inset-0 z-20   ">
+        <Image
+          src={"/images/bg.png"}
+          alt="Background"
+         fill
+          className="object-cover opacity-100  rounded-lg shadow-lg"
+        
+        />
+      
+      </div>
+      <div className="absolute top-[20%] left-[83%] -translate-x-1/2 z-20  w-full max-w-3xl px-4 text-center">
         <Image
           src={"/images/robo.png"}
           alt="Background"
@@ -32,13 +42,17 @@ export default function Home() {
 
       
  
-      <main className="relative z-20 font-medium pt-30  tracking-widest text-white  min-h-screen px-10 ">
+      <main className="relative z-20 font-medium pt-50  tracking-widest text-white  min-h-screen px-30 ">
     
-    <h1 className="font-mono text-3xl pb-25 t uppercase "><span className="text-indigo-300">Welcome,</span><br /> to the future of stock <br/> market analysis and  <br /> <span className="text-olive-300">AI-powered trend <br/> prediction</span></h1>
-    <div className="max-w-xl border-2 border-white/20 rounded-lg p-6  bg-white/10 backdrop-blur-3xl  ">
-    <p className="  text-mauve-400 text-xs font-mono uppercase tracking-widest ">Analyze market trends and predict future movements with our,<br /> advanced AI-powered platform.</p>
-        <p className="  text-mauve-400 text-xs font-mono uppercase tracking-widest">Advanced stock market analysis and trend,<br /> prediction using machine learning algorithms to help investors ,<br />make smarter and faster decisions.</p>
-      </div> 
+    <h1 className="font-mono text-3xl  font-bold   uppercase "><span className="text-indigo-300">Welcome,</span><br /> to the future of stock <br/> market analysis and  <br /> <span className="text-olive-300">AI-powered trend <br/> prediction</span>
+    
+    <p className="  text-mauve-400 text-xs font-mono uppercase tracking-widest ">Analyze market trends and predict future movements with our,<br /> advanced AI-powered platform.</p></h1>
+    
+
+              <div className=" pt-2">
+       <button className="border-0 border-white/20  bg-white/10 backdrop-blur-3xl uppercase shadow-lg hover:bg-white/30 text-white/90  py-2 px-4 rounded-sm transition duration-300 ease-in-out transform hover:scale-105 font-mono">Get Started {">"}</button>
+    </div>
+ 
       </main>
     </div>
   );
